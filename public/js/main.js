2de4af2f55f6226e95c32e0554dc12684522af66
Miguel Sicart <deterbold@gmail.com>
2024-06-19 18:28:43 +0100

    almost complete changes

--- public/js/main.js
+++ public/js/main.js
@@ -5,13 +5,14 @@ document.addEventListener("DOMContentLoaded", function () {
   video.setAttribute("playsinline", ""); // Required to work on iOS Safari
   const canvas = document.getElementById("canvas");
   const context = canvas.getContext("2d");
-  const toggleButton = document.getElementById("toggleButton");
-  toggleButton.textContent = "DALL·E 3";
-  toggleButton.classList.add("active");
   const captureButton = document.getElementById("capture");
+  const uploadButton = document.getElementById("uploadButton");
+  const modelButton = document.getElementById("modelButton");
+  const uploadInput = document.getElementById("uploadImage");
   let stream = null; // Variable to hold the stream reference
   const mobileOnlyButton = document.getElementById("mobileOnlyButton");
   let facingMode = "user";
+  let selectedModel = "dall-e-2"; // Default model is DALL·E 2
 
   // Access the webcam with the given facing mode
   function getCameraStream() {
@@ -25,7 +26,6 @@ document.addEventListener("DOMContentLoaded", function () {
         .then(function (mediaStream) {
           stream = mediaStream; // Store the stream reference
           video.srcObject = mediaStream;
-          //video.play(); // Ensure the video plays
         })
         .catch(function (error) {
           console.log("Error accessing the webcam", error);
@@ -56,23 +56,40 @@ document.addEventListener("DOMContentLoaded", function () {
     return randomNumber.toFixed(decimalPlaces);
   }
 
-  function displayImageAndDescription(imageUrl, description) {
+  function displayImageAndDescription(
+    originalImageUrl,
+    imageUrl,
+    originalDescription,
+    revisedDescription
+  ) {
+    const originalImageElement = document.getElementById("originalImage");
     const imageElement = document.getElementById("generatedImage");
     const reloadButton = document.getElementById("reloadButton");
+    const originalPromptElement = document.getElementById("originalPrompt");
     const descriptionElement = document.getElementById("imageDescription");
     const impactElement = document.getElementById("environmentalImpact");
     const footerElement = document.getElementById("pageFooter");
+    const sourcesElement = document.getElementById("sourcesElement");
 
-    if (imageElement) {
+    if (originalImageElement && imageElement) {
+      originalImageElement.src = originalImageUrl;
       imageElement.src = imageUrl;
       imageElement.onload = function () {
         // Ensure the image is loaded before displaying and setting widths
+        originalImageElement.style.display = "block"; // Show the original image
+        if (originalPromptElement) {
+          originalPromptElement.value = originalDescription; // Set the original prompt text
+          originalPromptElement.style.display = "block"; // Show the textarea
+          adjustTextAreaHeight(originalPromptElement); // Adjust the height
+        }
         imageElement.style.display = "block"; // Show the image
         reloadButton.style.display = "block"; // Show the reload button when the image is displayed
         footerElement.style.display = "block";
+
         // Set the description textarea width to match the image width
         if (descriptionElement) {
-          descriptionElement.value = description; // Set the description text
+          const rDescription = "Revised Prompt: " + revisedDescription;
+          descriptionElement.value = rDescription; // Set the description text
           descriptionElement.style.display = "block"; // Show the textarea
           adjustTextAreaHeight(descriptionElement); // Adjust the height if you have this function
         }
@@ -84,6 +101,10 @@ document.addEventListener("DOMContentLoaded", function () {
           impactElement.value = `For this interaction, ChatGPT has used approximately ${randomWaterUsage} ml of water (about half a espresso), and released approximately ${randomCO2}g of CO2 (a regular Google search uses around 0,2 g.).`;
           adjustTextAreaHeight(impactElement); // Adjust the height if you have this function
         }
+
+        if (sourcesElement) {
+          sourcesElement.style.display = "block";
+        }
       };
       // Event listener for the reload button
       reloadButton.addEventListener("click", function () {
@@ -93,62 +114,98 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   }
 
-  // Toggle button event listener
-  toggleButton.addEventListener("click", function () {
-    if (toggleButton.textContent.includes("2")) {
-      toggleButton.textContent = "DALL·E 3";
-      toggleButton.classList.add("active");
-      // Set the model to DALL·E 3
-    } else {
-      toggleButton.textContent = "DALL·E 2";
-      toggleButton.classList.remove("active");
-      // Set the model to DALL·E 2
-    }
-  });
-  // Capture button event listener
-  captureButton.addEventListener("click", function () {
+  function processImage(blob, originalImageUrl) {
+    const formData = new FormData();
+    formData.append("image", blob); // Append the image blob to the form data
+    formData.append("model", selectedModel); // Add the model selection based on the model button text
+
     // Show the loading animation
     document.getElementById("loadingAnimation").style.display = "flex";
-    document.getElementById("toggleContainer").style.display = "none";
-    captureButton.style.display = "none";
-    mobileOnlyButton.style.display = "none";
 
+    // Send the image to the server
+    fetch("/upload", {
+      method: "POST",
+      body: formData,
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        displayImageAndDescription(
+          originalImageUrl,
+          data.imageUrl,
+          data.description,
+          data.revisedPrompt
+        ); // Display the image and description
+        stopCamera(); // Stop the camera after taking the picture
+        // Hide the loading animation once the image is received
+        document.getElementById("loadingAnimation").style.display = "none";
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        document.getElementById("loadingAnimation").style.display = "none";
+      });
+  }
+
+  // Capture button event listener
+  captureButton.addEventListener("click", function () {
+    // Hide the elements
+    captureButton.style.display = "none";
+    uploadButton.style.display = "none";
+    modelButton.style.display = "none";
+    mobileOnlyButton.style.display = "none";
     video.style.display = "none"; // Hide the video element
 
     context.drawImage(video, 0, 0, canvas.width, canvas.height);
-    // Draw the video frame to the canvas
+    const originalImageUrl = canvas.toDataURL("image/jpeg");
+
     canvas.toBlob(function (blob) {
-      const formData = new FormData();
-      formData.append("image", blob); // Append the image blob to the form data
-      formData.append(
-        "model",
-        toggleButton.textContent.includes("2") ? "dall-e-2" : "dall-e-3"
-      ); // Add the model selection based on the toggle button text
-
-      // Send the image to the server
-      fetch("/upload", {
-        method: "POST",
-        body: formData,
-      })
-        .then((response) => response.json())
-        .then((data) => {
-          displayImageAndDescription(data.imageUrl, data.description); // Display the image and description
-          stopCamera(); // Stop the camera after taking the picture
-          //captureButton.style.display = "none"; // Hide the capture button
-
-          // Hide the loading animation once the image is received
-          document.getElementById("loadingAnimation").style.display = "none";
-        })
-        .catch((error) => {
-          console.error("Error:", error);
-          document.getElementById("loadingAnimation").style.display = "none";
-        });
+      processImage(blob, originalImageUrl);
     }, "image/jpeg");
+  });
+
+  // Upload button event listener
+  uploadButton.addEventListener("click", function () {
+    uploadInput.click(); // Trigger the file input click
+  });
+
+  // Handle image upload
+  uploadInput.addEventListener("change", function (event) {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = function (e) {
+        const originalImageUrl = e.target.result;
+
+        // Show the loading animation
+        document.getElementById("loadingAnimation").style.display = "flex";
+
+        // Hide the buttons and stop the webcam
+        captureButton.style.display = "none";
+        uploadButton.style.display = "none";
+        modelButton.style.display = "none";
+        mobileOnlyButton.style.display = "none";
+        stopCamera();
+        video.style.display = "none"; // Hide the video element
+
+        processImage(file, originalImageUrl);
+      };
+      reader.readAsDataURL(file);
+    }
   });
 
   mobileOnlyButton.addEventListener("click", function () {
     facingMode = facingMode === "user" ? "environment" : "user";
     getCameraStream(); // Re-fetch the camera stream with the new facing mode
+  });
+
+  // Model button event listener
+  modelButton.addEventListener("click", function () {
+    if (selectedModel === "dall-e-2") {
+      selectedModel = "dall-e-3";
+      modelButton.textContent = "Model: DALL·E 3";
+    } else {
+      selectedModel = "dall-e-2";
+      modelButton.textContent = "Model: DALL·E 2";
+    }
   });
 
   // Function to detect a mobile device
@@ -158,7 +215,6 @@ document.addEventListener("DOMContentLoaded", function () {
     return /android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini/i.test(
       userAgent
     );
-    console.log(userAgent);
   }
   if (isMobileDevice()) {
     console.log("mobile device");
